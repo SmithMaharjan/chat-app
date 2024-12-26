d0ed31f671bcb9903a7e84f297a6ff8e643f260e
SmithMaharjan <smith.maharjan1234@gmail.com>
2024-12-26 17:17:44 +0545

    setup express with socket.io and mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
 import express from "express"
 import http from "http"
 import { Server } from "socket.io"
-import { router as UserRouter } from "./src/route/user.route.js"
+import { router as AuthRouter } from "./src/route/auth.route.js"
 import dotenv from "dotenv"
+import { connectDB } from "./src/config/database.js"
 dotenv.config()
 
 const PORT = process.env.PORT
 
 const app = express()
+app.use(express.json())
 const server = http.createServer(app)
 const io = new Server(server)
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+    await connectDB()
     console.log(`the server is running ${PORT}`)
 })
-app.use(UserRouter)
+app.use(AuthRouter)
 
