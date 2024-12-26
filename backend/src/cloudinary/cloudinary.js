eec7d9bcd34910b49f5f8a7e7dfd09cb3a120d12
SmithMaharjan <smith.maharjan1234@gmail.com>
2024-12-26 17:19:16 +0545

    cloudinary integration for uploading files

---
+++ backend/src/cloudinary/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary"
+import { Readable } from "stream"
+import dotenv from "dotenv"
+dotenv.config()
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadToCloudinary = async (fileName, bufferFile) => {
+    return new Promise((resolve, reject) => {
+        const response = cloudinary.uploader.upload_stream({
+            "resource_type": "auto"
+        }, (error, result) => {
+            if (error) {
+                console.log(error)
+                reject(new Error(error))
+            }
+            resolve(result)
+
+        }
+        )
+        const bufferSteam = new Readable()
+        bufferSteam.push(bufferFile)
+        bufferSteam.push(null)
+        bufferSteam.pipe(response)
+
+    })
+
+}
+
