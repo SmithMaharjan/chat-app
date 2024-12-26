7e0e0628692eddde9771aee4b8a4c505df578a36
SmithMaharjan <smith.maharjan1234@gmail.com>
2024-12-26 20:49:11 +0545

    created login function with findByCredential method

--- backend/src/controller/auth.js
+++ backend/src/controller/auth.js
@@ -14,7 +14,7 @@ export const signUp = async (req, res) => {
     try {
 
         const user = await AuthService.signUp(fullname, username, email, password, confirmPassword, gender, response.secure_url)
-        generateTokenAndSetCookie(user._id, user.username, res)
+        generateTokenAndSetCookie(user._id, res)
 
         return res.status(200).json({ message: "user registered" })
     }
@@ -22,4 +22,22 @@ export const signUp = async (req, res) => {
         console.log(error.message)
         return res.status(200).json({ message: error.message })
     }
+}
+
+export const login = async (req, res) => {
+    const { usernameOrEmail, password } = req.body
+
+    try {
+        const user = await AuthService.login(usernameOrEmail, password)
+        generateTokenAndSetCookie(user._id, res)
+        return res.status(200).json({ message: "logged in" })
+
+    }
+    catch (error) {
+        console.log(error.message)
+        return res.status(400).json({ message: error.message })
+    }
+
+
+
 }
