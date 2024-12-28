90f85b2decb2e298dee3c04b26af6f21e91bb43d
SmithMaharjan <smith.maharjan1234@gmail.com>
2024-12-28 14:56:36 +0545

    added cookie-parser package to parse the cookie

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from "express"
 import http from "http"
 import { Server } from "socket.io"
 import { router as AuthRouter } from "./src/route/auth.route.js"
+import { router as messageRouter } from "./src/route/message.route.js"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import { connectDB } from "./src/config/database.js"
 dotenv.config()
 
@@ -10,6 +12,7 @@ const PORT = process.env.PORT
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 const server = http.createServer(app)
 const io = new Server(server)
 
@@ -18,4 +21,5 @@ server.listen(PORT, async () => {
     console.log(`the server is running ${PORT}`)
 })
 app.use(AuthRouter)
+app.use(messageRouter)
 
