134e8b04b19c5c46ff41a996e4ecb9103742f11e
SmithMaharjan <smith.maharjan1234@gmail.com>
2024-12-28 14:59:26 +0545

    create a middleware for security purpose by decode the token stored in cookie

---
+++ backend/src/middleware/protectRoutes.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken"
+import UserModel from "../model/user.js"
+
+export const protectedRoute = async (req, res, next) => {
+    console.log("hello")
+
+    const token = req.cookies.jwt
+    if (!token) {
+        throw new Error("cannot find token")
+    }
+    const decoded = jwt.verify(token, process.env.SECRET_TOKEN)
+    console.log(decoded, "decoded value")
+    if (!decoded) {
+        throw new Error("something went wrong")
+
+    }
+    const user = await UserModel.findById(decoded.id).select("-password")
+    if (!user) {
+        return res.status(400).json({ message: "user not found" })
+    }
+    console.log(user, "from middleware")
+    req.senderId = user
+    next()
+
+
+}
