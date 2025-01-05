38392e23e1817623c47ff33964449d362ab6f08f
SmithMaharjan <smith.maharjan1234@gmail.com>
2025-01-05 19:11:32 +0545

    added cors to connect the frontend and the backend

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 import express from "express"
+import cors from "cors"
 import http from "http"
-import { Server } from "socket.io"
-import { router as AuthRouter } from "./src/route/auth.route.js"
-import { router as messageRouter } from "./src/route/message.route.js"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import { Server } from "socket.io"
+import { router as AuthRouter } from "./src/route/auth.route.js"
+import { router as MessageRouter } from "./src/route/message.route.js"
+import { router as UserRouter } from "./src/route/user.route.js"
 import { connectDB } from "./src/config/database.js"
 dotenv.config()
 
 const PORT = process.env.PORT
+const corsOptions = {
+    origin: "http://localhost:5173",
+    optionsSuccessStatus: 200
+}
 
 const app = express()
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 const server = http.createServer(app)
@@ -21,5 +28,6 @@ server.listen(PORT, async () => {
     console.log(`the server is running ${PORT}`)
 })
 app.use(AuthRouter)
-app.use(messageRouter)
+app.use(MessageRouter)
+app.use(UserRouter)
 
